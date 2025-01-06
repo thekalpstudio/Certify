88107d6c55d02774f9bc6c1ac475ec9f36d80680
Nikhil <ftnikhil.eth@gmail.com>
2025-01-06 17:57:52 +0530

    enter email

--- certification/app/mint-sbt/page.js
+++ certification/app/mint-sbt/page.js
@@ -118,6 +118,20 @@ const MintSbt = () => {
               />
             </div>
 
+            {/* User Email Input */}
+            <div className="mt-4">
+              <label className="block text-sm font-medium mb-2 text-black">
+                User Email <span className="text-red-500">*</span>
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 text-black"
+                placeholder="Enter user email"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+
             {/* Organization Input */}
             <div className="mt-4">
               <label className="block text-sm font-medium mb-2 text-black">
